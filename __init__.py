f56acff2e281c4ad1ac435a7ef2cb7e50e1a7d55
Rahul Verma <rahul.verma@farfetch.com>
2021-05-21 02:39:43 +0530

    [FIX] reformat pep8

--- __init__.py
+++ __init__.py
@@ -9,19 +9,23 @@ from database_connection import DatabaseConnection
 from slot_notifier import SlotNotifier
 
 # Tweak the parameters in this function as per your requirement. By default fetches availability for all ages, all vaccines and all doses (first and second)
+
+
 def get_available_slots_by_state_id_and_dates(scheme, hostname, headers, state_id, district_ids, dates, start_time, dose_1=True, dose_2=True, age_limits=[18, 45], vaccines=["covaxin", "covishield", "sputnik"]):
     if not district_ids:
-        district_ids = cowin_gateway.get_districts_by_state_id(scheme, hostname, headers, state_id)
+        district_ids = cowin_gateway.get_districts_by_state_id(
+            scheme, hostname, headers, state_id)
     available_slots = []
     api_calls = 1
     for date in dates:
         for district_id in district_ids:
-            centers = cowin_gateway.get_vaccine_centers_by_district_and_date(scheme, hostname, headers, district_id, date)
+            centers = cowin_gateway.get_vaccine_centers_by_district_and_date(
+                scheme, hostname, headers, district_id, date)
             api_calls += 1
             for center in centers:
                 for slot in center["sessions"]:
                     if slot["min_age_limit"] in age_limits and \
-                       ((dose_1 and slot["available_capacity_dose1"]>0) or (dose_2 and slot["available_capacity_dose2"]>0)) and \
+                       ((dose_1 and slot["available_capacity_dose1"] > 0) or (dose_2 and slot["available_capacity_dose2"] > 0)) and \
                        slot["vaccine"].lower() in vaccines:
                         available_slot = vaccine_slot.VaccineSlot(date=slot["date"],
                                                                   age_limit=slot["min_age_limit"],
@@ -30,16 +34,19 @@ def get_available_slots_by_state_id_and_dates(scheme, hostname, headers, state_i
                                                                   pincode=center["pincode"],
                                                                   state_id=state_id,
                                                                   district_id=district_id,
-                                                                  state_name=center["state_name"].lower(),
-                                                                  center_name=center["name"],
-                                                                  center_id=center["center_id"],
-                                                                  available_capacity_dose1=slot["available_capacity_dose1"],
-                                                                  available_capacity_dose2=slot["available_capacity_dose2"])
+                                                                  state_name=center["state_name"].lower(
+                        ),
+                            center_name=center["name"],
+                            center_id=center["center_id"],
+                            available_capacity_dose1=slot["available_capacity_dose1"],
+                            available_capacity_dose2=slot["available_capacity_dose2"])
                         available_slots.append(available_slot)
     print "Total api calls = %s" % (api_calls)
     # Order by age limit, vaccine, district, date, pincode, available doses
-    sorted_slots = sorted(available_slots, key=lambda slot: (slot.age_limit, slot.vaccine, slot.district_name, slot.date, slot.pincode, slot.available_capacity_dose1, slot.available_capacity_dose2))
+    sorted_slots = sorted(available_slots, key=lambda slot: (slot.age_limit, slot.vaccine, slot.district_name,
+                                                             slot.date, slot.pincode, slot.available_capacity_dose1, slot.available_capacity_dose2))
     return sorted_slots
+
 
 def update_records_in_database(available_slots):
     connection = DatabaseConnection().get_connection()
@@ -49,10 +56,12 @@ def update_records_in_database(available_slots):
         cursor.execute(query)
         connection.commit()
 
+
 def get_updated_records_from_database(start_time, vaccine, state_name, age_limit, dose_1=False, dose_2=False):
     records = []
 
-    query = vaccine_slot.VaccineSlot.get_updated_records_query(start_time, vaccine, state_name, age_limit, dose_1, dose_2)
+    query = vaccine_slot.VaccineSlot.get_updated_records_query(
+        start_time, vaccine, state_name, age_limit, dose_1, dose_2)
     if query:
         connection = DatabaseConnection().get_connection()
         cursor = connection.cursor()
@@ -64,6 +73,7 @@ def get_updated_records_from_database(start_time, vaccine, state_name, age_limit
         cursor.close()
     return records
 
+
 def get_formatted_message(record, dose_1=False, dose_2=False):
     # message_object = namedtuple('X', record.keys())(*record.values())
     message = ""
@@ -74,10 +84,9 @@ def get_formatted_message(record, dose_1=False, dose_2=False):
            dose_2 and key == "available_capacity_dose1":
             continue
         value = record[key]
-        message += "<b>%s: </b> %s \n" %(key, value)
+        message += "<b>%s: </b> %s \n" % (key, value)
 
     return message
-
 
 
 if __name__ == "__main__":
@@ -115,14 +124,18 @@ if __name__ == "__main__":
                 for age_limit in config.age_limits:
                     for dose in config.vaccines[vaccine][age_limit]:
                         # get recent updated records from database for covaxin 18
-                        updated_slots = get_updated_records_from_database(start_time=start_time, vaccine=vaccine, state_name=state, age_limit=age_limit, **{dose: config.vaccines[vaccine][age_limit][dose]})
-                        print "Updated records for %s, %s, %s" %(vaccine, age_limit, dose)
+                        updated_slots = get_updated_records_from_database(
+                            start_time=start_time, vaccine=vaccine, state_name=state, age_limit=age_limit, **{dose: config.vaccines[vaccine][age_limit][dose]})
+                        print "Updated records for %s, %s, %s" % (vaccine, age_limit, dose)
                         for record in updated_slots:
                             print "Sending message for record"
-                            message = get_formatted_message(record, **{dose: config.vaccines[vaccine][age_limit][dose]})
+                            message = get_formatted_message(
+                                record, **{dose: config.vaccines[vaccine][age_limit][dose]})
                             print message
-                            slot_notifier.send_message(age_limit, vaccine, state, message, dose)
+                            slot_notifier.send_message(
+                                age_limit, vaccine, state, message, dose)
         end_time = datetime.datetime.now().strftime(date_format)
-        diff = datetime.datetime.strptime(end_time, date_format) - datetime.datetime.strptime(start_time, date_format)
-        print "Total time taken = %s seconds" %(diff.total_seconds())
+        diff = datetime.datetime.strptime(
+            end_time, date_format) - datetime.datetime.strptime(start_time, date_format)
+        print "Total time taken = %s seconds" % (diff.total_seconds())
         count += 1
--- database_connection.py
+++ database_connection.py
@@ -1,19 +1,21 @@
 from database_config import config
 import pymysql
 
+
 class DatabaseConnection:
     def __init__(self):
-        self.host=config["host"]
-        self.user=config["user"]
-        self.password=config["password"]
-        self.database=config["database"]
+        self.host = config["host"]
+        self.user = config["user"]
+        self.password = config["password"]
+        self.database = config["database"]
 
     def get_connection(self):
-        connection = pymysql.connect(user=self.user, passwd=self.password, host=self.host, database=self.database)
+        connection = pymysql.connect(
+            user=self.user, passwd=self.password, host=self.host, database=self.database)
         return connection
 
-    def __enter__(self): 
+    def __enter__(self):
         return self
 
-    def __exit__(self): 
+    def __exit__(self):
         pass
