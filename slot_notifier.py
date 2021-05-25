0cc9b9eadf0689da79363e7089be575c2fdfa307
Rahul Verma <rahul.verma@farfetch.com>
2021-05-25 20:27:13 +0530

    [FIX] use segregated modules in slot notifier

--- slot_notifier.py
+++ slot_notifier.py
@@ -1,5 +1,5 @@
 import telegram
-import telegram_config
+from config.telegram import bot_tokens, chat_ids
 from decorators import retry
 
 
@@ -9,7 +9,7 @@ class SlotNotifier:
     def _get_chat_id(self, age_limit, vaccine, state, dose):
         chat_id_key = "%s_%s_%s_%s" % (
             age_limit, vaccine.lower(), state.lower(), dose)
-        if chat_id_key not in telegram_config.chat_ids:
+        if chat_id_key not in chat_ids:
             raise Exception(
                 "Invalid chat id %s. Please ensure the channel is added in config" % (chat_id_key))
         return chat_id_key
@@ -20,7 +20,7 @@ class SlotNotifier:
         if 'sputnik' in vaccine.lower():
             vaccine = "sputnik"  # don't know the exact name cowin are using
         chat_id_key = self._get_chat_id(age_limit, vaccine, state, dose)
-        chat_id = telegram_config.chat_ids[chat_id_key]
+        chat_id = chat_ids[chat_id_key]
         try:
             # print "Sending message for chat_id %s" %(chat_id_key)
             bot.sendMessage(
@@ -31,7 +31,7 @@ class SlotNotifier:
             raise Exception("Exception = %s. chat_id=%s" % (e, chat_id_key))
 
     def _get_token_from_config(self):
-        all_tokens = telegram_config.bot_tokens
+        all_tokens = bot_tokens
         token_index = SlotNotifier.message_counter % len(all_tokens)
         token = all_tokens[token_index]  # pick tokens based on the token_number generated to avoid hitting api limits
         return token
